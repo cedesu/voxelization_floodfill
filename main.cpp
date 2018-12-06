91828fb7ed1ff66789500fbbc0396bf61e8b62e5
cedesu <34418825+cedesu@users.noreply.github.com>
2018-12-06 11:19:22 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -110,7 +110,7 @@ int main(int argc, char *argv[])
   	printf("%s\n",x);
   	fl=x;
   	fi="../../Thingi10K/raw_meshes/"+fl; 
-  	if (j>=6623&&j<7000){
+  	if (j>=0&&j<4000){
   igl::readSTL(fi,V,F,N);
   if ((F.rows()<50000)&&!(F.rows()<5000&&V.rows()<10000)){
   	
