a8f92a6b328c8f28f837f5bbbad7e37315a581fb
cedesu <34418825+cedesu@users.noreply.github.com>
2018-12-06 23:08:52 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -110,11 +110,11 @@ int main(int argc, char *argv[])
   	printf("%s\n",x);
   	fl=x;
   	fi="../../Thingi10K/raw_meshes/"+fl; 
-  	if (j>=6172&&j<8000){
+  	if (j>=5950&&j<6000){
   igl::readSTL(fi,V,F,N);
-  if ((F.rows()<10000)&&!(F.rows()<5000&&V.rows()<10000)){
+ // if ((F.rows()<10000)&&!(F.rows()<5000&&V.rows()<10000)){
 
-  //if ((F.rows()<5000&&V.rows()<10000)){
+  if ((F.rows()<5000&&V.rows()<10000)){
   	
 Eigen::MatrixXi T;
 igl::AABB<Eigen::MatrixXd,3> tree;
